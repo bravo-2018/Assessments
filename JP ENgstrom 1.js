f4c26a7cf0836a5d774ce86dee48bcc3a7e9dcc9
JP Engstrom <jpengst@gmail.com>
2018-03-30 11:56:15 -0700

    Initial Commit

--- JP ENgstrom 1.js
+++ JP ENgstrom 1.js
@@ -9,13 +9,13 @@ if(true) {
 // 2. Create a for loop that counts to 10 backwards. Comment each line to explain what is happening.
 
 for( ) {
-	
+
 // your code here
-	
+
 }
-	
+
 // 3. Create an array with at least 4 items inside it. Console.log the second thing in the list. Change the fourth item to equal "duck".
-    
+
 
 // 4. Being comfortable with if/else decisions is very helpful in code. It helps you get into the practice of breaking complex actions down into their simpler parts. You might recognize this example:
 
@@ -25,7 +25,7 @@ if(!tired) {
 	console.log("I need coffee!")
 }
 
-// Now try to come up with your own if/else decision - if you can't think of one, try to create one for opening a door. 
+// Now try to come up with your own if/else decision - if you can't think of one, try to create one for opening a door.
 
 
 // 5. Time to combine all this! Create a function that takes in two numbers as arguements. Find the sum of those two numbers, if the sum is less than 20, console log the sum. If the two numbers sum to more than 20, console log "can't count that high!"
