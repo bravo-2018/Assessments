b6f575c49f1e04b220ed4fdbf11561fb7d577601
David Yi <yidavid@yahoo.com>
2018-04-05 20:43:21 -0700

    Week-2.js completion

--- Week-2.js
+++ Week-2.js
@@ -1,6 +1,28 @@
-// 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three". If it is not, print that the number "is not divisble by three".
+// 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three". If it is not, print that the number "is not divisble by three"
 
+function divisibleby3(var num)
+{
+   if (var%3 == 0)
+   {
+     console.log(var + " is divisble by 3")
+   }
+   else
+   { console.log(var + " is not divisible by 3")}
+}
+
+divisibleby3(5)
+divisibleby3(6)
 // 2. Write about yourself using an object. Include at least three properties of you and store your object in a variable with your name.
+var mySelf = {
+    firstName: "David",
+    lastName: "Yi",
+    race: "Male",
+    getFullName: function() {
+      return (this.firstName + " " + this.lastName)
+    }
+}
+
+
 
 // 3. Given the object below, fill in how to find specific information:
 
@@ -15,29 +37,53 @@ var bicycle = {
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log just the bell from the list of gear
-console.log()
+var justBell = []
+
+for (var k=0; k < bicycle.gear.length; k++){
+  if (bicycle.gear[k].toLowerCase().includes("bell"))
+  {
+    justBell = bicycle.gear[k]
+  }
+}
+
+console.log(justBell)
 
 // Log the correct PSI for the tires
-console.log()
+var correctPSI = []
+for (var j=0; j < bicycle.wheels.specs.length; j++)
+{
+   if (bicycle.wheels.specs[j].toLowerCase().includes("psi"))
+   {
+     correctPSI = bicycle.wheels.specs[j].split(" ")
+   }
+}
 
-// 4. Have the function AlphabetSoup(str) take the "str" parameter being passed and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume no numbers or punctuation symbols will not be included in the parameter. 
+console.log(correctPSI[0])
+
+// 4. Have the function AlphabetSoup(str) take the "str" parameter being passed and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume no numbers or punctuation symbols will not be included in the parameter.
 
 // Input:"hooplah"
 // Output:"ahhloop"
 
 function AlphabetSoup(str) {
+  var outStr
 
-    // your code goes here  
-	
-	return str; 
+  for (var m = str.length; m >0; m--)
+  {
+     outStr += str[m]
+  }
+
+	return outStr;
 }
 
-// keep this function call here 
+AlphabetSoup("hooplah")
 
-AlphabetSoup(readline());   
+// keep this function call here
+
+AlphabetSoup(readline());
 
 // 5. Given the arrays below, use a for loop to print one value from each array concatenated together. How would your code need to change to accomodate arrays of different lengths?
 
@@ -46,3 +92,47 @@ var nums = [1, 5, 88, 2, 5, 42, 57, 101]
 var nouns = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a"]
 
 // output of the first function should be: "1 ducks"
+function concatTwoArrays(numsArray,nounsArray)
+{
+  if (numsArray.length != nounsArray.length){
+    if (numsArray.length > nounsArray.length)
+    {
+       //console.log("Number array is bigger padding nouns array with EMPTY string")
+       for (var m = 0 ; m < (numsArray.length - nounsArray.length) + 1 ; m++)
+       {
+          nounsArray.push(" ")
+       }
+       console.log("New nounsArray : " + nounsArray)
+
+    }
+    else if (nounsArray.length > numsArray.length)
+    {
+       console.log("Noun array is bigger, padding num array with 0")
+       for (var m = 0 ; m < (nounsArray.length - numsArray.length) + 1; m++)
+       {
+          numsArray.push(0)
+       }
+       console.log("New numsArray : " + numsArray)
+    }
+  }
+
+  var returnArray = []
+  for (var k = 0; k < numsArray.length; k++)
+  {
+     returnArray[k] = numsArray[k] + " " + nounsArray[k]
+  }
+  return returnArray
+}
+
+// Test concatTwoArrays when numArray and nounsArray is same size
+var nums = [1, 5, 88, 2, 5, 42, 57, 101]
+var nouns = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a"]
+console.log(concatTwoArrays(nums,nouns))
+
+// Test concatTwoArrays when numArray size is bigger than nounsArray size
+var nums1 = [1, 5, 88, 2, 5, 42, 57, 101, 9413,394]
+console.log(concatTwoArrays(nums1,nouns))
+
+// Test concatTwoArrays when nounsArray size is bigger than numsArray size
+var nouns1 = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a","TEST", "TEST1"]
+console.log(concatTwoArrays(nums,nouns1))
--- Week-2.md
+++ Week-2.md
@@ -2,53 +2,64 @@
 
 These are potential interview questions. Try your best to answer each question on your own before looking up the answer online.
 
-#### 1. Name all of the data types in Javascript, mark which are primitives. 
+#### 1. Name all of the data types in Javascript, mark which are primitives.
 
   //Your Answer
-  
-  
+  String, number, boolean, null, array,object
+
   //Googled Answer
+  string, number, boolean, null, undefined, symbol (new in ECMAScript 2015).
 
-
-#### 2. Look at this Javascript and try to predict, what will the browser's console show? 
+#### 2. Look at this Javascript and try to predict, what will the browser's console show?
 
 ``` javascript
 
-var text = 'outside';
+//var text = 'outside';
+var aa = "outside"
 function logIt(){
-    console.log(text);
-    var text = 'inside';
+  //  var text = 'inside'
+    console.log(aa);
+    console.log("T#EST")
+    var aa = "inside";
 };
 
 logIt();
 
 ```
 
-first Guess: 
+first Guess:
+'outside'
 
 
-Then, past the code in your console and explain why you were right/wrong. 
+Then, past the code in your console and explain why you were right/wrong.
+
+My answer is wrong as output was "undefined"..   There is varialbe name conflict
+with same name "text"
 
 
 #### 3. What is JSON? How does it relate to javascript objects?
 
   //Your Answer
-  
-  
+   I am not sure as we have not covered about JSON yet..
+
   //Googled Answer
+  JSON (JavaScript Object Notation) is a lightweight data-interchange format. It is easy for humans to read and write. It is easy for machines to parse and generate. It is based on a subset of the JavaScript Programming Language
 
 
 #### 4. Describe a closure, what is it good for and how do you recognize one?
 
   //Your Answer
-  
-  
+  block.. which can be recongnized by {}
+
+
   //Googled Answer
-  
+  A closure is an inner function that has access to the outer (enclosing) function's variables—scope chain. The closure has three scope chains: it has access to its own scope (variables defined between its curly brackets), it has access to the outer function's variables, and it has access to the global variables.Feb 2, 2013
 
 #### 5. What's the difference between =, ==, and === in JavaScript?
 
   //Your Answer
-  
-  
+  =  assigment operator
+  == comparison
+  === strong comparison
+
   //Googled Answer
