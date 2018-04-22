20b997df1299335db59ad8b72274d8cee6c421fd
DenisePillette <denisedpillette@gmail.com>
2018-04-22 14:32:21 -0700

    adding week 3 and 4, and renamed week 0 and 1

--- Week-3.js
+++ Week-3.js
@@ -1,6 +1,17 @@
 1. Write a simple React component that simply prints "I am a component" to the screen. Be sure to include all necessary imports, exports, etc...
 
-2. Practice With Loops: In React, we often use the map function to iterate over an array held in state. To get more practice with this, write a vanilla javascript for loop that outputs the same thing as the map function below. 
+import React from 'react';
+
+class simpleComponent extends React.Component {
+  render(){
+    return React.createElement("h1", null, "I am a component")
+  }
+}
+
+export default simpleComponent;
+
+
+2. Practice With Loops: In React, we often use the map function to iterate over an array held in state. To get more practice with this, write a vanilla javascript for loop that outputs the same thing as the map function below.
 
 
 var stuffArray = [42, "Arthur Dent", 1978, "Zaphod", "Vogon", "Marvin, the Paranoid Android"]
@@ -9,13 +20,24 @@ stuffArray.map(function(el, i){
   console.log(el + " is at index: " + i)
 })
 
-// your for loop here, you can reference the same stuffArray variable
-
-
-3. Basic sorting: Find the cheapest price -- you are given a list of prices and need to find the signle lowest price from the array. Write a function that takes in an array of numbers, and returns the lowest price
-
-var steal = function(){
-
-  // your logic here
-  
+for (var i = 0; i < stuffArray.length; i++) {
+  console.log(stuffArray[i] + " is at index: " + i)
 }
+
+
+3. Basic sorting: Find the cheapest price -- you are given a list of prices and need to find the single lowest price from the array. Write a function that takes in an array of numbers, and returns the lowest price
+
+var numArray = [100, 99, 5000, 5, 999, 111, 123456789]
+
+var steal = function(array){
+  array.sort(function(a, b){return a-b});
+  return array[0]
+}
+
+steal(numArray)
+
+var stealTwo = function(array){
+  return Math.min(...array)
+}
+
+stealTwo(numArray)
--- Week-4.rb
+++ Week-4.rb
@@ -2,29 +2,78 @@
 
 tempArray = [1, 2, 6, 9, 3, 21]
 
-# your ruby loop here
+tempArray.each do |i|
+  puts i * 2
+end
 
 #### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
 
 # your three built in ruby methods
 
-#### 3. Look at this horrible ruby code, and fix it to be good ruby code. 
+# Method 1
+100.times do
+    if count(i) % 3 == 0 && count(i) % 5 == 0
+            print "FizzBuzz \n"
+            i = count(i)
+    elsif count(i) % 3 == 0
+            print "Fizz \n"
+            i = count(i)
+    elsif count(i) % 5 == 0
+            print "Buzz \n"
+            i = count(i)
+    else
+            print "#{count(i)} \n"
+            i = count(i)
+    end
+end
 
-``` ruby
-class example
+#Method 2
+def sum_these_numbers(i,s)
+    i + s
+end
+
+def is_even(i)
+    if i % 2 == 0
+        print "True"
+    else
+        print "False"
+    end
+
+#Method 3
+
+array = ["Star Wars", "Pulp Fiction", "Cinderella", "Indiana Jones"]
+array2 = array[0..2]
+
+i = 0
+def count(i)
+    i = i + 1
+end
+
+20.times do
+    print "#{count(i)} \n"
+    i = count(i)
+end
+
+
+
+#### 3. Look at this horrible ruby code, and fix it to be good ruby code.
+
+
+class Example
   def initialize(day)
-    @day=day
+    @day = day
   end
-  
-  def Say_hi
-    if(day == "Friday"){
+
+  def say_hi
+    if @day == "Friday"
       puts "TGIF!"
-    }
-    elseif(day == "Monday"){
-      puts "Its monday again"
-    }
+    elsif @day == "Monday"
+      puts "It's Monday again"
     else
       puts "another day"
+    end
   end
 end
-```
+
+example = Example.new("Monday")
+puts example.say_hi
