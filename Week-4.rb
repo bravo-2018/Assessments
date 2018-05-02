17e89654f3e8df5d6feeff61408f97e6138f30eb
Trinh Huynh <36995369+trinhhuynh5@users.noreply.github.com>
2018-05-01 20:20:38 -0700

    Update Week-4.rb

--- Week-4.rb
+++ Week-4.rb
@@ -9,11 +9,18 @@ print newArray
 
 
 #### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
+chomp([rs]), chomp!([rs])
+Removes off the line ending from the value of the variable
 
+gsub(pattern[, replace]), gsub!(pattern[, replace])
+Searches a string held in the variable $_ for a pattern, and if found, replaces all the occurrence of the pattern with the replace and returns the replaced string. gsub! modifies the original string in place, gsub makes copy, and keeps the original unchanged.
 
+rand(max)
+Returns a random integer number greater than or equal to 0 and less than the value of max. (max should be positive.) Automatically calls srand unless srand() has already been called.
+If max is 0, rand returns a random float number greater than or equal to 0 and less than 1
 
-
-#### 3. Look at this horrible ruby code, and fix it to be good ruby code. 
+  
+  #### 3. Look at this horrible ruby code, and fix it to be good ruby code. 
 
 ``` ruby
 class example
@@ -33,3 +40,4 @@ class example
   end
 end
 ```
+
